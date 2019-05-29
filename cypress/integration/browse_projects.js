075cddc0af00deb7db9d45cd34c49510959b518f
Adam De Fouw <aldefouw@medicine.wisc.edu>
2019-05-29 13:44:08 -0500

    More refactoring [TIG-308]

--- cypress/integration/browse_projects.js
+++ cypress/integration/browse_projects.js
@@ -1,40 +1,16 @@
 describe('Browse Projects', function () {
 
-    function checkCellClassName(col_name, values = []){
-        abstractSort(col_name, 'table#table-proj_table tr:first span', values, 1);
-    }
-
-    function checkCellValue(col_name, element, values = []){
-        abstractSort(col_name, element, values);
-    }
-
-    function abstractSort(col_name, element, values, klass = 0){
-        cy.get('table#table-proj_table tr:first span').should('not.contain', "Loading").then(() => {
-                cy.get('th div').contains(col_name).click().then(()=> {
-                cy.get(element).then(($a) => { 
-                    klass ? expect($a).to.have.class(values[0]) : expect($a).to.contain(values[0]);   
-                    cy.get('th div').contains(col_name).click().then(()=>{
-                        cy.get(element).then(($e) => {
-                            klass ? expect($e).to.have.class(values[1]) : expect($e).to.contain(values[1]);                                       
-                        })
-                    })
-                })
-            })
-        })
-    }
-
     beforeEach(() => {
         cy.visit('/').then(() => {
             cy.get('a').contains('Control Center').click()
             cy.get('a').contains('Browse Projects').click()
-            cy.get('button').contains('View all projects').click()
         })
     })
 
     describe('Display Projects', function () { 
 
         it('displays a list of all projects', function () {
-            //Check to see if there are the correct number of projects initially.
+            cy.get('button').contains('View all projects').click()
             cy.get('table#table-proj_table').find('tr:visible').should('have.length', 13)
         })
 
@@ -44,6 +20,7 @@ describe('Browse Projects', function () {
                      cy.get('table#table-proj_table tr:first span').should('not.contain', "Loading").then(() => {
                         cy.get('table#table-proj_table tr:first div.projtitle').then(($a) => {
                             expect($a).to.contain("Test Project")
+                            cy.get('table#table-proj_table').find('tr:visible').should('have.length', 1)
                         })
                      })
                 })
@@ -52,18 +29,38 @@ describe('Browse Projects', function () {
 
 
         it('displays the projects for Test User when you click on the user after entering the username', function () {
-           
-
+           cy.get('input#user_search').type('test_user').then(() => {            
+                cy.get('ul#ui-id-1 li a').click().then(($a) => {
+                    cy.get('table#table-proj_table tr:first span').should('not.contain', "Loading").then(() => {
+                         cy.get('table#table-proj_table tr:first div.projtitle').then(($a) => {
+                            expect($a).to.contain("Test Project")
+                            cy.get('table#table-proj_table').find('tr:visible').should('have.length', 1)
+                        })
+                    })
+                })
+            })
         }) 
 
         it('displays the projects for Test User when you click on the user after entering the email address', function () {
-           
-           
+            cy.get('input#user_search').type('test_user@example.com').then(() => {            
+                cy.get('ul#ui-id-1 li a').click().then(($a) => {
+                    cy.get('table#table-proj_table tr:first span').should('not.contain', "Loading").then(() => {
+                         cy.get('table#table-proj_table tr:first div.projtitle').then(($a) => {
+                            expect($a).to.contain("Test Project")
+                            cy.get('table#table-proj_table').find('tr:visible').should('have.length', 1)
+                        })
+                    })
+                })
+            })
         })  
 
     })
 
     describe('Filter Projects', function () {   
+
+        beforeEach(() => {
+            cy.get('button').contains('View all projects').click()
+        })
 
         it('filters project by title', function () {
 
@@ -91,8 +88,34 @@ describe('Browse Projects', function () {
 
     })
     
-
     describe('Sort Columns', function () {   
+
+        function checkCellClassName(col_name, values = []){
+            abstractSort(col_name, 'table#table-proj_table tr:first span', values, 1);
+        }
+
+        function checkCellValue(col_name, element, values = []){
+            abstractSort(col_name, element, values);
+        }
+
+        function abstractSort(col_name, element, values, klass = 0){
+            cy.get('table#table-proj_table tr:first span').should('not.contain', "Loading").then(() => {
+                    cy.get('th div').contains(col_name).click().then(()=> {
+                    cy.get(element).then(($a) => { 
+                        klass ? expect($a).to.have.class(values[0]) : expect($a).to.contain(values[0]);   
+                        cy.get('th div').contains(col_name).click().then(()=>{
+                            cy.get(element).then(($e) => {
+                                klass ? expect($e).to.have.class(values[1]) : expect($e).to.contain(values[1]);                                       
+                            })
+                        })
+                    })
+                })
+            })
+        }
+
+        beforeEach(() => {
+            cy.get('button').contains('View all projects').click()
+        })
 
         it('sorts the Project Title column appropriately', function () {
         checkCellValue('Project Title', 
