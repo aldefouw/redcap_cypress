1190fe4db4ef713027a6aa150d304ef1609002d5
Adam De Fouw <aldefouw@medicine.wisc.edu>
2019-05-29 12:47:51 -0500

    Refactor of code [TIG-308]

--- cypress/integration/browse_projects.js
+++ cypress/integration/browse_projects.js
@@ -1,44 +1,24 @@
 describe('Browse Projects', function () {
 
-    function testColumnSortByClassName(col_name, klasses = []){
-        cy.get('table#table-proj_table tr:first span').should('not.contain', "Loading").then(() => {
-                cy.get('th div').contains(col_name).click().then(()=> {
-            
-
-                cy.get('table#table-proj_table tr:first span').then(($a) => { 
-
-                    expect($a).to.have.class(klasses[0]);  
-
-                    cy.get('th div').contains(col_name).click().then(()=>{
-
-                        cy.get('table#table-proj_table tr:first span').then(($e) => {
-
-                            expect($e).to.have.class(klasses[1]);                           
-
-                        })
-
-                    })
-                })
-
-            })
-        })
+    function checkCellClassName(col_name, values = []){
+        abstractSort(col_name, 'table#table-proj_table tr:first span', values, 1);
     }
 
-    function testColumnSortByValue(col_name, element, values = []){
+    function checkCellValue(col_name, element, values = []){
+        abstractSort(col_name, element, values);
+    }
+
+    function abstractSort(col_name, element, values, klass = 0){
         cy.get('table#table-proj_table tr:first span').should('not.contain', "Loading").then(() => {
-            cy.get('th div').contains(col_name).click().then(()=> {            
-                cy.get('table#table-proj_table tr:first ' + element).then(($a) => { 
-
-                    expect($a).to.contain(values[0]);  
-
+                cy.get('th div').contains(col_name).click().then(()=> {
+                cy.get(element).then(($a) => { 
+                    klass ? expect($a).to.have.class(values[0]) : expect($a).to.contain(values[0]);   
                     cy.get('th div').contains(col_name).click().then(()=>{
-                        cy.get('table#table-proj_table tr:first ' + element).then(($e) => {
-
-                            expect($e).to.contain(values[1]);                           
-
+                        cy.get(element).then(($e) => {
+                            klass ? expect($e).to.have.class(values[1]) : expect($e).to.contain(values[1]);                                       
                         })
                     })
-                })               
+                })
             })
         })
     }
@@ -81,27 +61,37 @@ describe('Browse Projects', function () {
     });
 
     it('sorts the Project Title column appropriately', function () {
-        testColumnSortByValue("Project Title", 'div.projtitle', ["Basic Demography", "Test Project"]);
+        checkCellValue('Project Title', 
+                       'table#table-proj_table tr:first div.projtitle', 
+                        ['Basic Demography', 'Test Project']);
     });
 
     it('sorts the Records column appropriately', function () {
-        testColumnSortByClassName("Records", ['pid-cnti-1', 'pid-cnti-13']);
+        checkCellClassName('Records', 
+                           ['pid-cnti-1', 'pid-cnti-13']);
     });
 
     it('sorts the Fields column appropriately', function () {
-        testColumnSortByValue("Fields", 'div', ["2", "198"]);
+        checkCellValue('Fields', 
+                       'table#table-proj_table tr:first div', 
+                       ['2', '198']);
     });
 
     it('sorts the Instrument column appropriately', function () {
-        testColumnSortByValue("Instrument", 'div.fc span div', ["1 survey", "15 forms"]);
+        checkCellValue('Instrument', 
+                       'table#table-proj_table tr:first div.fc span div', 
+                       ['1 survey', '15 forms']);
     });
 
     it('sorts the Type column appropriately', function () {
-        testColumnSortByValue("Type", 'span.hidden', ["0", "1"]);
+        checkCellValue('Type', 
+                       'table#table-proj_table tr:first span.hidden', 
+                       ['0', '1']);
     });
 
     it('sorts the Status column appropriately', function () {
-        testColumnSortByClassName("Status", ["glyphicon-check", "glyphicon-wrench"]);
+        checkCellClassName('Status', 
+                           ['glyphicon-check', 'glyphicon-wrench']);
     });
 
     it('displays the projects for Test User', function () {
