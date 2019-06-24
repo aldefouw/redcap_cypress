33df1d44bf148465a682ef954ca7c2cabd076fda
Adam De Fouw <aldefouw@medicine.wisc.edu>
2019-06-24 09:55:33 -0500

    Try this

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -65,6 +65,7 @@ Cypress.Commands.add("mysql_db", (type, replace = '') => {
         console.log(cmd)
 
     cy.exec(cmd).then((response) => {
+        cy.writeFile('~/log_of_mysql_command_' + type + '.txt', response)
         console.log(response)
     })
 })
