c7b74a8a1168e94985f4b1ad1d9928ec03193427
Adam De Fouw <aldefouw@medicine.wisc.edu>
2019-06-24 10:31:43 -0500

    See if we can record the command more easily

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -65,7 +65,7 @@ Cypress.Commands.add("mysql_db", (type, replace = '') => {
         console.log(cmd)
 
     cy.exec(cmd).then((response) => {
-        cy.writeFile('~/log_of_mysql_command_' + type + '.txt', response)
+        cy.writeFile('log_of_mysql_command' + type + '.txt', response)
         console.log(response)
     })
 })
