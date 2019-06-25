b6cb1786588da7de772aa3bdf8e2e0fdce801799
Adam De Fouw <aldefouw@medicine.wisc.edu>
2019-06-25 11:20:29 -0500

    Add project folder and SQL script
    This would be the folder to test individual project specs ...

---
+++ cypress/integration/projects/example.js
@@ -0,0 +1,14 @@
+describe('Project Example', () => {
+
+	before(() => {
+
+	    //Fetch the hooks SQL seed data
+	    cy.mysql_db('/projects/example') 
+	
+	})
+
+    it('Your project spec here', () => {
+
+    })
+
+})
