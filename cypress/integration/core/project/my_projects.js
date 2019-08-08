09f6c61dbfaaae7d468b983e1485335c53b2c13c
Adam De Fouw <aldefouw@medicine.wisc.edu>
2019-08-08 15:31:21 -0500

    Begin test of My Projects

--- cypress/integration/core/project/my_projects.js
+++ cypress/integration/core/project/my_projects.js
@@ -1,10 +1,18 @@
 describe('My Projects', () => {
 
-    it('Test spec here', () => {
-            
+	before(() => {
+	    cy.visit_v({page: '', user_type: 'standard'})
+	})
+
+    it('Should display columns: Project Title, Records, Fields, Instruments, Type, and Status', () => {
+		cy.visit('?action=myprojects').then(() => {                
+			cy.get('div').contains('Project Title')
+			cy.get('div').contains('Records')
+			cy.get('div').contains('Fields')
+			cy.get('div').contains('Instrument')    
+			cy.get('div').contains('Type')
+			cy.get('div').contains('Status')        
+		}) 
     })
 
-    it('Test spec here', () => {
-            
-    })
 })
