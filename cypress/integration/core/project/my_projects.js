20988f09d239614c2e9257a8e021d20548be5d8a
Adam De Fouw <adefouw@gmail.com>
2019-08-09 12:51:25 -0500

    Make it possible to easily switch between user types

--- cypress/integration/core/project/my_projects.js
+++ cypress/integration/core/project/my_projects.js
@@ -1,11 +1,11 @@
 describe('My Projects', () => {
 
 	before(() => {
-	    cy.visit_v({page: '', user_type: 'standard'})
+		cy.set_user_type('standard')
 	})
 
     it('Should display columns: Project Title, Records, Fields, Instruments, Type, and Status', () => {
-		cy.visit('?action=myprojects').then(() => {                
+		cy.visit_base({url: '?action=myprojects'}).then(() => {                
 			cy.get('div').contains('Project Title')
 			cy.get('div').contains('Records')
 			cy.get('div').contains('Fields')
