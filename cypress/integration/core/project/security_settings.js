39594be4a15494eccc6cfd2b52953835528722c2
Shefali Mukerji <mukerji2@yahoo.com>
2019-08-27 09:42:53 -0500

    updated tests

--- cypress/integration/core/project/security_settings.js
+++ cypress/integration/core/project/security_settings.js
@@ -1,4 +1,4 @@
-describe('Admin Security Settings', () => {
+describe('System Security Settings', () => {
 
 	before(()=> {
 		cy.set_user_type('admin')
@@ -18,30 +18,41 @@ describe('Admin Security Settings', () => {
     		expect($div).to.contain('Your system configuration values have now been changed!')
     	})
 	})
-})
-
-
-describe('User Security Settings' , () => {
-
-	before(()=> {
-		cy.get('ul').contains('Log out').click({force: true})
-		cy.set_user_type('standard')
-		cy.visit_version({page: 'index.php'})	
-	})
 
 	it('Should display system offline message when user logs in', () => {
+		cy.get('ul').contains('Log out').click({force: true})
+		cy.set_user_type('standard')
+		cy.visit_base({url: 'index.php'})
 		cy.get('div').should(($div) => {
 			expect($div).to.contain('REDCap is currently offline. Please return at another time. We apologize for any inconvenience.')
 		})
 	})
-	
+
+	it('Should display system offline message when admin logs in', () => {
+		cy.get('ul').contains('Log out').click({force: true})
+		cy.set_user_type('admin')
+		cy.visit_base({url: 'index.php'})
+		cy.get('div').should(($div) => {
+			expect($div).to.contain('REDCap and all its projects are currently OFFLINE and are not accessible to normal users. You can return the REDCap system back to ONLINE status in the Control Center.')
+		})
+	})
+
+	it('Should change system status back to "SYSTEM ONLINE"', () => {
+		cy.visit_version({page: 'ControlCenter/general_settings.php'})
+		cy.get('select').contains('SYSTEM ONLINE').click({force: true})
+    	cy.get('input').contains('Save Changes').click({force: true})
+    	cy.get('div').should(($div) => {
+    		expect($div).to.contain('Your system configuration values have now been changed!')
+    	})
+	})
+
 })
 
-describe('Project Settings', () => {
+describe('Project Security Settings', () => {
 
 	before(() => {
 		cy.set_user_type('admin')
-		cy.visit_version({page: 'ControlCenter/edit_project.php?project=5'})
+		cy.visit_version({page: 'ControlCenter/edit_project.php', params: "project=5"})
 	})
 
 	it('Should save changes after project status is changed from online to offline', () => {
@@ -51,5 +62,14 @@ describe('Project Settings', () => {
 			expect($div).to.contain('Your changes have been saved!')
 		})
 	})
+
+	it('Should display offline next to project title when user logs in', () => {
+		cy.get('ul').contains('Log out').click({force: true})
+		cy.set_user_type('standard')
+		cy.visit_version({page: 'index.php', params: "pid=5"})
+		cy.get('div').should(($div) => {
+			expect($div).to.contain('REDCap is currently offline. PLease return at another time. We apologize for any inconvenience.')
+		})
 	})
+})
 
