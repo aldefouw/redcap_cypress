a587ba68712cce5b81a088d0121acfb58f507d63
Shefali Mukerji <mukerji2@yahoo.com>
2019-08-29 15:52:35 -0500

    strated tests for entry_via_instruments.js

--- cypress/integration/core/data/entry_via_instrument.js
+++ cypress/integration/core/data/entry_via_instrument.js
@@ -1,5 +1,12 @@
 describe('Data Entry through the Data Collection Instrument', () => {
-
+	before(() => {
+		cy.set_user_type('admin')
+		cy.visit_version({page: 'ControlCenter/validation_type_setup.php'})
+		cy.get('button').contains('Enable').then(($btn) => {
+			$btn.click({force: true})
+		})
+		
+	})
     it('Test spec here', () => {
             
     })
