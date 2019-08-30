8922468027cd2a997cdcbd6c1b8067928723435b
Adam De Fouw <aldefouw@medicine.wisc.edu>
2019-08-30 12:15:11 -0700

    More updates

--- download-windows-dependencies.bat
+++ download-windows-dependencies.bat
@@ -7,6 +7,8 @@ for /f "tokens=1 skip=1 delims=-" %%c in ('wmic os get osarchitecture') do (
 )
 :BREAK
 
+curl -o sed.zip https://codeload.github.com/mbuilov/sed-windows/zip/master
+
 rem 64 Bit System
 IF %os_bits% == 64 (
 
@@ -18,9 +20,11 @@ rem 32 Bit System
 
 	curl -o mysql-cli.zip -L https://downloads.mysql.com/archives/get/file/mysql-shell-1.0.11-windows-x86-32bit.zip
 	set folder=mysql-shell-1.0.11-windows-x86-32bit
-	
+
 )
 
+tar zxvf sed.zip
 tar zxvf mysql-cli.zip
 
 ren .\%folder%\ mysql
+ren .\sed-windows-master\ sed
--- test_db/db.bat
+++ test_db/db.bat
@@ -1,4 +1,4 @@
-rem echo off
+echo off
 
 set db_cmd=%1 --sql --host=%2 --port=%3 --user=%5 --password=%6
 
@@ -9,10 +9,30 @@ set sql_file=%sql_file:/=\%
 set sql=%cd%\test_db\%sql_file%.sql
 set tmp=%cd%\test_db\%sql_file%.sql.tmp
 
-rem IF [%8]==[] (
+set first=s/REDCAP_DB_NAME/$4/g
 
-rem ) ELSE (
+rem Get the Operating System bits
 
-rem )
+for /f "tokens=1 skip=1 delims=-" %%c in ('wmic os get osarchitecture') do (
+	set os_bits=%%c
+	GOTO BREAK
+)
+:BREAK
+
+rem 64 Bit System
+IF %os_bits% == 64 (
+	
+	IF [%8]==[] (
+		type %sql% > ..\sed\sed-4.7-x64.exe %first% > %tmp%
+	) ELSE (
+		type %sql% > ..\sed\sed-4.7-x64.exe %first% > ..\sed\sed-4.7-x64.exe s/%8/g > %tmp%
+	)
+
+rem 32 Bit System
+) ELSE (
+
+	echo 'test'
+
+)
 
 %db_cmd% < %tmp%
