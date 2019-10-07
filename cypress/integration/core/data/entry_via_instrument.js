7b8a11a358d2a6e90d2198d94e24a335252152d2
Shefali Mukerji <mukerji2@yahoo.com>
2019-10-07 16:39:11 -0500

    edited entry_via_instrument.js

--- cypress/integration/core/data/entry_via_instrument.js
+++ cypress/integration/core/data/entry_via_instrument.js
@@ -1,13 +1,34 @@
 describe('Data Entry through the Data Collection Instrument', () => {
 	before(() => {
 		cy.set_user_type('admin')
-		cy.visit_version({page: 'ControlCenter/validation_type_setup.php'})
-		cy.get('button').contains('Enable').then(($btn) => {
-			$btn.click({force: true})
-		})
-		
+		cy.visit_version({page: 'Design/define_events.php', params: "pid=13"})
+		cy.get('a').contains('Add New Arm').click({force: true })
+		cy.get('input#arm_name').type('Arm 2')
+		cy.get('input#savebtn').click({force: true})
+		cy.get('input#descrip').type('Event 1')
+		cy.get('input#addbutton').click({force: true})
+		cy.visit_version({page: 'Design/define_events.php', params: "pid=13"})
+		cy.get('a').contains('Arm 2').click({force: true})
+		cy.get('input#descrip').type('Event 2')
+		cy.get('input#addbutton').click({force: true})
+		cy.visit_version({page: 'Design/define_events.php', params: "pid=13"})
+		cy.get('a').contains('Arm 2').click({force: true})
+		cy.get('input#descrip').type('Event 3')
+		cy.get('input#addbutton').click({force: true})	
+		cy.visit_version({page: 'Design/online_designer.php', params: "pid=13"})
+		cy.get('span').contains('Create').parent().parent().click({force: true})
+		cy.get('input#new_form-my_first_instrument').type('Field Types')
+		cy.get('input').contains('Create').click({force: true})
 	})
-    it('Test spec here', () => {
+    
+    it('Should allow new record to be added to Arm 1', () => {
+    	cy.set_user_type('standard')	
+    	cy.visit_version({page: 'DataEntry/record_home.php', params: "pid=13"})
+    	cy.get('select').contains('Arm 1: Arm 1').parent().select('Arm 1: Arm 1')
+    	cy.get('button').contains('Add new record').click({force: true})
+    	cy.get('b').contains('is a new Record ID').should(($b) => {
+    		expect($b).to.contain('Record "1"')
+    	})
             
     })
 
