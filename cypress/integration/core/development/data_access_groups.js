30ba1d093f361a328cb5928c5928480f629fb88b
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-01-07 13:22:18 -0600

    Add stubbed layout for Data Access Group specs

--- cypress/integration/core/development/data_access_groups.js
+++ cypress/integration/core/development/data_access_groups.js
@@ -1,10 +1,39 @@
 describe('Data Access Groups (DAGs)', () => {
 
-    it('Test spec here', () => {
-            
-    })
+	before(() => {
+		cy.set_user_type('admin')
+	})
 
-    it('Test spec here', () => {
-            
-    })
+	describe('User Interface', () => {
+
+		it('Should have the ability to create data access groups', () => {
+		            
+	    })
+
+	    it('Should have the ability to delete data access groups', () => {
+	            
+	    })
+
+	    it('Should have the ability to provide a unique DAG group name in the data export CSV or label', () => {
+
+	    })
+
+	    it('Should have the ability to assign data instruments to a specific data access group', () => {
+	    	
+	    })
+
+	    describe('Data Restriction Abilities', () => {
+
+		    it('Should have the ability to restrict a user to the data they entered', () => {
+		            
+		    })
+
+		    it('Should have the ability to restrict a user to the data of the same DAG', () => {
+		            
+		    })
+
+	    })
+
+	})    
+
 })
