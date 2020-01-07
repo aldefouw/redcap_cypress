e01f11b883cc00499af73ffba53586896ee678d7
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-01-07 15:37:47 -0600

    Fix additoinal bad stub architecture

--- cypress/integration/core/pre-requisite/add_manage_users.js
+++ cypress/integration/core/pre-requisite/add_manage_users.js
@@ -8,27 +8,27 @@ describe('Add / Manage Users', () => {
 
 		describe('Users', () => {
 
-			it('Should have the ability to create an individual user' => {
+			it('Should have the ability to create an individual user', () => {
 
 			})
 
-			it('Should have the ability to suspend an individual user' => {
+			it('Should have the ability to suspend an individual user', () => {
 				
 			})
 
-			it('Should have the ability to unsuspend an individual user' => {
+			it('Should have the ability to unsuspend an individual user', () => {
 				
 			})
 
-			it('Should have the ability to delete an individual user' => {
+			it('Should have the ability to delete an individual user', () => {
 				
 			})
 
-			it('Should have the ability to view all users in a tabular form' => {
+			it('Should have the ability to view all users in a tabular form', () => {
 				
 			})
 
-			it('Should have the ability to search for an individual user' => {
+			it('Should have the ability to search for an individual user', () => {
 				
 			})
 
@@ -36,11 +36,11 @@ describe('Add / Manage Users', () => {
 
 		describe('Security & Authentication', () => { 
 
-			it('Should have the ability to lock out users after a certain number of failed login attempts' => {
+			it('Should have the ability to lock out users after a certain number of failed login attempts', () => {
 				
 			})
 
-			it('Should have the ability to specify the amount of time a user will be locked out after failed login attempts' => {
+			it('Should have the ability to specify the amount of time a user will be locked out after failed login attempts', () => {
 				
 			})
 
