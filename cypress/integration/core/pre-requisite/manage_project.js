9776b1255bae606316c0bf8e58da28898bfd194d
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-01-07 10:01:38 -0600

    Add stubbed layout for Manage Project specs

--- cypress/integration/core/pre-requisite/manage_project.js
+++ cypress/integration/core/pre-requisite/manage_project.js
@@ -1,11 +1,110 @@
 describe('Manage Project Creation, Deletion, Settings', () => {
 
-    it('Test spec here', () => {
-            
-    })
+	describe('Control Center', () => {
 
-    it('Test spec here', () => {
-            
-    })
+		it('Should have the ability to limit creation of new projects to administrators', () => {
+
+		})
+
+		it('Should have the ability to limit the moving of projects to production to administrators', () => {
+
+		})
+
+		it('Should have the ability to enable users to edit survey responses', () => {
+
+		})
+
+		it('Should have the ability to enable Draft Mode changes to be automatically approved under certain conditions', () => {
+
+		})
+
+		it('Should have the ability to limit adding or modifying events and arms while in Production mode to administrators', () => {
+
+		})
+	})
+
+	describe('User Interface - General', () => {
+
+		it('Should have the ability to create new projects from a blank slate', () => {
+
+		})
+
+		it('Should have the ability to customize / modify existing Project Title', () => {
+
+		})
+
+		it('Should have the ability to designate the purpose of the project', () => {
+
+		})
+
+		describe('Copy Functionality', () => {
+
+			it('Should have the ability to copy the project with neither data nor users included', () => {
+
+			})
+
+			it('Should have the ability to copy the project with data included but without users', () => {
+
+			})
+
+			it('Should have the ability to copy the project with both data and users included', () => {
+
+			})
+		})		
+	})
+
+	describe('User Interface - Longitudinal Project Settings', () => {
+
+		it('Should have the ability to enable and disable Longitudinal Data Collection', () => {
+
+		})
+
+		it('Should have the ability to designate data collection instruments for defined events for each arm', () => { 
+
+		})
+
+		it('Should have the ability to define unique event schedules for each arm', () => { 
+
+		})
+
+		it('Should have the ability to create repeating events and instruments', () => { 
+
+		})
+
+		it('Should require administrator approval to delete events for longitudinal projects while in Production mode', () => { 
+
+		})		
+	})
+
+	describe('User Interface - Survey Project Settings', () => {
+
+		it('Should have the ability to enable and disable survey functionality at the project level', () => { 
+
+		})
+
+		it('Should have the ability to enable and disable each data collection instrument in a project as a survey', () => { 
+
+		})
+
+		it('Should have the ability to set the survye status to active or offline', () => { 
+
+		})
+
+		it('Should have the ability to create repeating surveys', () => { 
+
+		})		
+	})
+
+	describe('User Interface - Survey distribution', () => {
+
+		it('Should have the ability to create a public survey link when the survey is in the first instrument position', () => { 
+
+		})
+
+		it('Should have the ability to create a designated email field', () => { 
+
+		})
+		
+	})
 
 })
