c4b93846a876820cf1f2b118952e67b4101bc579
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-01-07 13:02:17 -0600

    Add stubbed layout for Project Status specs

--- cypress/integration/core/project/status.js
+++ cypress/integration/core/project/status.js
@@ -1,10 +1,81 @@
 describe('Project Status', () => {
 
-    it('Test spec here', () => {
-            
-    })
+	before(() => {
+		cy.set_user_type('standard')
+	})
 
-    it('Test spec here', () => {
+	describe('Control Center', () => {
+
+	    it('Should have the ability to limit "Move to Production" capabilities to administrators only', () => {
             
-    })
+    	})
+
+	})
+
+	describe('User Interface', () => {
+
+		describe('Development as Initial Status', () => {
+
+			it('Should have the ability to change the project status from Development to Production and keep collected data', () => {
+
+			})
+
+			it('Should have the ability to change the project status from Development to Production and delete all data, logging, and survey responses', () => {
+
+			})
+
+			it('Should have the ability to change the project status from Development to Archive', () => {
+
+			})
+
+		})
+
+		describe('Production as Initial Status', () => {
+
+			it('Should have the ability to change the project status from Production to Inactive', () => {
+
+			})
+
+			it('Should have the ability to change the project status from Production to Archive', () => {
+
+			})
+
+			it('Should have the ability to change the project status from Production to Development (for administrators)', () => {
+				cy.set_user_type('admin')
+				cy.set_user_type('standard')
+			})
+		})
+
+		describe('Inactive as Initial Status', () => {
+
+			it('Should have the ability to change the project status from Inactive to Production', () => {
+
+			})
+
+			it('Should have the ability to change the project status from Inactive to Archive', () => {
+
+			})
+
+			it('Should have the ability to change the project status from Inactive to Development (for administrators)', () => {
+				cy.set_user_type('admin')
+				cy.set_user_type('standard')
+			})
+
+		})
+
+		describe('Archive as Initial Status', () => {
+
+			it('Should have the ability to change the project status from Archive to Production', () => {
+
+			})
+
+			it('Should have the ability to change the project status from Archive to Development (for administrators)', () => {
+				cy.set_user_type('admin')
+				cy.set_user_type('standard')
+			})
+
+		})
+
+	})
+
 })
