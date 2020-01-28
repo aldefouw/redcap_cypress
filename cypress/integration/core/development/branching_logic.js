30d941d586251d0c0cb895c2ae3489a12defe11f
Shefali Mukerji <mukerji2@yahoo.com>
2020-01-28 13:03:41 -0600

    completed branching_logic.js

--- cypress/integration/core/development/branching_logic.js
+++ cypress/integration/core/development/branching_logic.js
@@ -4,8 +4,8 @@ describe('Branching Logic', () => {
 		cy.visit_version({page: 'ProjectSetup/index.php', params: 'pid=5'})
 		cy.get('button#setupEnableSurveysBtn').click({force:true})
 		cy.visit_version({page: 'ProjectSetup/other_functionality.php', params: "pid=5"})
+		cy.wait(1000)
         cy.get('button').contains('development status').click({force:true})
-        cy.wait(1000)
         cy.visit_version({page: 'Design/online_designer.php', params: "pid=5"})
         cy.get('button').contains('Enable').click({force:true})
         cy.get('button').contains('Save Changes').click({force:true})
@@ -15,11 +15,11 @@ describe('Branching Logic', () => {
         cy.get('input').eq(96).click({force:true})
         cy.get('textarea#advBranchingBox').type('[first_name]!=""')
         cy.get('button').contains('Save').click({force:true})
+        cy.get('button').contains('Close').click({force:true})
 	})
 
 	describe('User Interface', () => {
         before(() => {
-            cy.set_user_type('standard')
             cy.visit_version({page: 'DataEntry/record_home.php', params: "pid=5"})
             cy.get('button').contains('Add new record').click({force:true})
         })
@@ -39,14 +39,12 @@ describe('Branching Logic', () => {
 
 	describe('Survey Interface', () => {
         before(() => {
-            cy.set_user_type('admin')
             cy.get('a').contains('Manage Survey Participants').click({force:true})
             cy.get('button').contains('Leave without saving changes').click({force:true})
             cy.get('input#longurl').invoke('attr', 'value').then(($val) =>{
                 cy.visit($val)
             })
         })
-
 	    it('Should have the ability to show a field ONLY when specific conditions are met', () => {
             cy.get('input[name=first_name]').type('John')
             cy.get('input[name=email]').type('John@wisc.edu')
