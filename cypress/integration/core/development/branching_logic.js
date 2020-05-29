320b7533354033784e19d5abf42fa471d17c83d8
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-05-29 17:01:32 -0500

    Fix problems with branching logic tests [RED-1840]

--- cypress/integration/core/development/branching_logic.js
+++ cypress/integration/core/development/branching_logic.js
@@ -1,27 +1,32 @@
 describe('Branching Logic', () => {
 	before(() => {
+		cy.mysql_db('projects/pristine')
 		cy.set_user_type('admin')
+
 		cy.visit_version({page: 'ProjectSetup/index.php', params: 'pid=5'})
-		cy.get('button#setupEnableSurveysBtn').click({force:true})
+		cy.get('button#setupEnableSurveysBtn').click()
 		cy.visit_version({page: 'ProjectSetup/other_functionality.php', params: "pid=5"})
-		cy.wait(1000)
-        cy.get('button').contains('development status').click({force:true})
-        cy.visit_version({page: 'Design/online_designer.php', params: "pid=5"})
-        cy.get('button').contains('Enable').click({force:true})
-        cy.get('button').contains('Save Changes').click({force:true})
-        cy.get('a').contains('Basic Demography Form').click({force:true})
-        cy.get('img').eq(64).parent().click({force:true})
-        cy.wait(1000)
-        cy.get('input').eq(96).click({force:true})
-        cy.get('textarea#advBranchingBox').type('[first_name]!=""')
-        cy.get('button').contains('Save').click({force:true})
-        cy.get('button').contains('Close').click({force:true})
+        cy.get('button').contains('development status').click()
+
+        cy.get('body', ).should(($body) => {
+        	expect($body).to.contain('The project is now back in development status.')
+        }).then(() => {
+			cy.visit_version({page: 'Design/online_designer.php', params: "pid=5"})
+			cy.get('button').contains('Enable').click()
+			cy.get('button').contains('Save Changes').click()
+			cy.get('a').contains('Basic Demography Form').click()
+			cy.find_online_designer_field("Last Name").parent().parentsUntil('tr').find('img[title="Branching Logic"]').parent().click()
+			cy.get('textarea#advBranchingBox').type('[first_name]!=""')
+			cy.get('button').contains('Save').click()
+			cy.get('button').contains('Close').click()
+        })
+
 	})
 
 	describe('User Interface', () => {
         before(() => {
             cy.visit_version({page: 'DataEntry/record_home.php', params: "pid=5"})
-            cy.get('button').contains('Add new record').click({force:true})
+            cy.get('button').contains('Add new record').click()
         })
 	    it('Should have the ability to show a field ONLY when specific conditions are met', () => {
             cy.get('input[name=first_name]').type('John')
@@ -31,6 +36,7 @@ describe('Branching Logic', () => {
             })
             cy.get('input[name=first_name]').clear()
             cy.get('input[name=email]').clear()
+
             cy.get('tr#last_name-tr').should(($tr) => {
                 expect($tr).to.have.css('Display', 'none')
             })
@@ -39,8 +45,8 @@ describe('Branching Logic', () => {
 
 	describe('Survey Interface', () => {
         before(() => {
-            cy.get('a').contains('Manage Survey Participants').click({force:true})
-            cy.get('button').contains('Leave without saving changes').click({force:true})
+            cy.get('a').contains('Survey Distribution Tools').click()
+            cy.get('button').contains('Leave without saving changes').click()
             cy.get('input#longurl').invoke('attr', 'value').then(($val) =>{
                 cy.visit($val)
             })
