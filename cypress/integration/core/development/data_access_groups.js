b29641256518c4c435f367ee8575f09b1b04a3e2
Shefali Mukerji <mukerji2@wisc.edu>
2020-06-16 13:05:46 -0500

    started data access groups tests

--- cypress/integration/core/development/data_access_groups.js
+++ cypress/integration/core/development/data_access_groups.js
@@ -2,16 +2,32 @@ describe('Data Access Groups (DAGs)', () => {
 
 	before(() => {
 		cy.set_user_type('standard')
+		cy.mysql_db('projects/pristine')
 	})
 
 	describe('User Interface', () => {
+		
+		before(() => {
+			cy.set_user_type('admin')
+			cy.visit_version({page: 'DataAccessGroups/index.php', params: 'pid=1'})
+		})
 
 		it('Should have the ability to create Data Access Groups', () => {
-		            
+					cy.get('input#new_group').type('Test Group')
+					cy.get('button#new_group_button').click()
+					cy.get('div#dags_table').should(($div) => {
+						expect($div).to.contain('Test Group')
+					})
 	    })
 
 	    it('Should have the ability to delete Data Access Groups', () => {
-	            
+	            cy.get('tr').contains('Test Group').parent().parent().parent().within(($tr) => {
+					cy.get('img').click()
+				})
+				cy.get('button').contains('Delete').click()
+				cy.get('div#dags_table').should(($div) => {
+					expect($div).not.to.contain('Test Group')
+				})
 	    })
 
 	    it('Should have the ability to provide a unique Data Access Group name in the data export CSV or label', () => {
