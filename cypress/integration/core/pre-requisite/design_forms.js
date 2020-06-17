d2799500079e3ef46dcb42952f423ca05db55a2a
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-06-17 13:57:34 -0500

    Additional updates to test specifications

--- cypress/integration/core/pre-requisite/design_forms.js
+++ cypress/integration/core/pre-requisite/design_forms.js
@@ -39,124 +39,175 @@ describe('Design Forms using Data Dictionary & Online Designer', () => {
 			// Check for identifiers
 		})
 
+		describe('Data Collection Instruments', () => {
 
-		it('Should allow a new instrument to be created', () => {
-
-		})
-
-		it('Should allow an instrument to be renamed', () => {
-
-		})
-
-		it('Should allow an instrument to be deleted', () => {
-
-		})
-
-		it('Should allow instruments to be reordered', () => {
-
-		})
-
-		it('Should contains all of the expected field types', () => {
-			// Text Box
-			// Notes Box
-			// Calculated Field
-			// Multiple Choice – Drop Down
-			// Multiple Choice – Radio Buttons
-			// Checkboxes
-			// Signature
-			// File Upload
-			// Descriptive Text
-			// Begin New Section				
-		})
-
-		describe('Field Types', () => {
-
-
-			describe('Text Box', () => {
-
-
-
+			it('Should allow a new instrument to be created', () => {
 
 			})
 
-			describe('Notes', () => {
-
-
-
+			it('Should allow an instrument to be renamed', () => {
 
 			})
 
-			describe('Calculated Field', () => {
-
-				
-
-				
-			})
-
-			describe('Calculated Field', () => {
-
-				
-
-				
-			})
-
-			describe('Multiple Choice', () => {
-
-				
-				describe('Dropdown', () => {
-
-				}
-
-				describe('Radio', () => {
-
-				}
-				
-			})
-
-			describe('Checkboxes', () => {
-
+			it('Should allow an instrument to be deleted', () => {
 
 			})
 
-			describe('Signature', () => {
-				
+			it('Should allow instruments to be reordered', () => {
 
 			})
 
+			describe('Field Types', () => {
 
-			describe('File Upload', () => {
+
+				it('Should contain all of expected field types', () => {
+					// Text Box
+					// Notes Box
+					// Calculated Field
+					// Multiple Choice – Drop Down
+					// Multiple Choice – Radio Buttons
+					// Checkboxes
+					// Signature
+					// File Upload
+					// Descriptive Text
+					// Begin New Section				
+				})
+
+				it('Should not allow invalid names', () => {
 				
+				})
 
+				it('Should allow reordering of fields', () => {
+					
+				})
+
+				it('Should allow renaming of a field', () => {
+					
+				})
+
+				it('Should allow copying of a field', () => {
+					
+				})
+
+				it('Should allow a field to be marked as an identifier', () => {
+					
+				})
+
+				describe('Text Box', () => {
+
+					it('Should allow the creation of this field type', () => {
+
+					})
+
+				})
+
+				describe('Notes', () => {
+
+					it('Should allow the creation of this field type', () => {
+
+					})
+
+				})
+
+				describe('Calculated Field', () => {
+
+					it('Should allow the creation of this field type', () => {
+
+					})
+
+				})
+
+				describe('Calculated Field', () => {
+
+					it('Should allow the creation of this field type', () => {
+
+					})		
+
+				})
+
+				describe('Multiple Choice', () => {
+
+					describe('Dropdown', () => {
+						it('Should allow the creation of this field type', () => {
+
+						})		
+
+						it('Should automatically populate raw values for choices', () => {
+
+						})
+					})
+
+					describe('Radio', () => {
+						it('Should allow the creation of this field type', () => {
+
+						})	
+
+						it('Should automatically populate raw values for choices', () => {
+
+						})	
+					})
+					
+				})
+
+				describe('Checkboxes', () => {
+
+					it('Should allow the creation of this field type', () => {
+
+					})	
+
+					it('Should automatically populate raw values for choices', () => {
+
+					})
+
+				})
+
+				describe('Signature', () => {
+
+					it('Should allow the creation of this field type', () => {
+
+					})	
+
+				})
+
+
+				describe('File Upload', () => {
+
+					it('Should allow the creation of this field type', () => {
+
+					})	
+
+				})
+
+				describe('Descriptive Text', () => {
+					
+					it('Should allow the creation of this field type', () => {
+
+					})	
+
+					it('Should allow an attached image', () => {
+
+					})	
+
+
+					it('Should allow an attached audio clip', () => {
+
+					})	
+
+				})
+
+				describe('Begin New Section', () => {
+
+					it('Should allow the creation of this field type', () => {
+
+					})	
+
+					it('Should not allow this to be the first field of the form', () => {
+
+					})	
+
+				})
 			})
-
-			describe('Descriptive Text', () => {
-				
-
-			})
-
-			describe('Begin New Section', () => {
-				
-
-			})
-
 		})
-
-		it('Should allow reordering of fields', () => {
-			
-		})
-
-		it('Should allow renaming of a field', () => {
-			
-		})
-
-		it('Should allow copying of a field', () => {
-			
-		})
-
-		it('Should allow a field to be marked as an identifier', () => {
-			
-		})
-
     })
 
 	describe ('Data Dictionary', () => {
@@ -169,7 +220,5 @@ describe('Design Forms using Data Dictionary & Online Designer', () => {
 			
 		})
 
-
 	})
-
 })
