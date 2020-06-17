237f8c5ce7e49734fb1f2caebe517653db8d7278
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-06-17 11:28:27 -0500

    Update design forms stubs

--- cypress/integration/core/pre-requisite/design_forms.js
+++ cypress/integration/core/pre-requisite/design_forms.js
@@ -1,7 +1,43 @@
 describe('Design Forms using Data Dictionary & Online Designer', () => {
 
 	before(() => {
+		//Reset the projects back to what they should be
+		cy.mysql_db('projects/pristine')
 		
 	})
+
+    describe('Online Designer', () => {
+
+    	before(() => {
+    		cy.set_user_type('standard')
+			
+			//Visit Classic Database 
+			cy.visit_version({page: 'Design/online_designer.php', params: "pid=13"})
+    	})
+
+
+		it('Should contain Project Setup permisisons for current user', () => {
+
+
+		})
+
+		it('Should show the project without surveys used and in development mode', () => {
+
+
+		})
+
+		it('Should show you the appropriate options for viewing and designing your data collection instruments', () => {
+
+			// Online Designer
+			// Data Dictionary
+			// REDCap Shared Library
+			// Download PDF of all instruments
+			// Download current data dictionary
+			// Check for identifiers
+			
+		})
+
+
+    })
 
 })
