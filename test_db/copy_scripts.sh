2fcb24465d67c567d8a572f9322e43c4403356a0
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-10-02 17:27:03 -0500

    More updates

--- test_db/copy_scripts.sh
+++ test_db/copy_scripts.sh
@@ -2,7 +2,6 @@ redcap_version="$1"
 source_location="/Users/aldefouw/Dev/redcap/redcap-source"
 
 cypress_location="$PWD/test_db"
-truncate_sql="${cypress_location}/truncate_tables.sql"
 seed_sql="${cypress_location}/cypress_data.sql"
 db_prefix_sql="${cypress_location}/structure_prefix.sql"
 
@@ -13,6 +12,9 @@ data_sql="${sql_path}/install_data.sql"
 #CREATE STRUCTURE FILE
 structure_and_data_file="${cypress_location}/structure_and_data.sql"
 version_substitution="s/'4.0.0'),/'${redcap_version}'),/g"
+
+rm $structure_and_data_file
+rm "${$structure_and_data_file}.tmp"
 
 cat $db_prefix_sql > $structure_and_data_file
 cat $install_sql >> $structure_and_data_file
