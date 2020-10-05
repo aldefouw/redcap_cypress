56cac852dcd47bc1feac9865d97a3bcf62a15151
Adam De Fouw <aldefouw@medicine.wisc.edu>
2020-10-05 13:40:40 -0500

    Working entirely now

--- test_db/copy_scripts.sh
+++ test_db/copy_scripts.sh
@@ -1,4 +1,7 @@
+# DEFINE REDCAP VERSION (FIRST PARAMETER)
 redcap_version="$1"
+
+# DEFINE PATHS
 source_location="/Users/aldefouw/Dev/redcap/redcap-source"
 
 cypress_location="$PWD/test_db"
@@ -13,9 +16,11 @@ data_sql="${sql_path}/install_data.sql"
 structure_and_data_file="${cypress_location}/structure_and_data.sql"
 version_substitution="s/REDCAP_VERSION_MAGIC_STRING/${redcap_version}/g"
 
+#REMOVE EXISTING STRUCTURE AND DATA FILE
 rm $structure_and_data_file
 rm "${structure_and_data_file}.tmp"
 
+#CREATE NEW STRUCTURE AND DATA FILE
 cat $db_prefix_sql > $structure_and_data_file
 cat $install_sql >> $structure_and_data_file
 cat $data_sql >> $structure_and_data_file
