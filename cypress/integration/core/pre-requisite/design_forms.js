eb091d1c085b9bbb93213f97793d2f40bf595429
Adam De Fouw <aldefouw@medicine.wisc.edu>
2021-08-24 14:26:25 -0500

    Begin work on Online Designer tests |  Issue #22

--- cypress/integration/core/pre-requisite/design_forms.js
+++ cypress/integration/core/pre-requisite/design_forms.js
@@ -3,57 +3,143 @@ describe('Design Forms using Data Dictionary & Online Designer', () => {
 	before(() => {
 		//Reset the projects back to what they should be
 		cy.mysql_db('projects/pristine')
-		
+
+		//Set the standard user permission
+		cy.set_user_type('standard')
+
+		//Go to the main page of the project
+		cy.visit_version({page: 'index.php', params: "pid=13"})
+	})
+
+	describe('Prerequisite', () => {
+
+		it('Should contain Project Setup permissions for current user', () => {
+			cy.get('a').contains('User Rights').click()
+
+			cy.get('div#user_rights_roles_table').should(($table) => {
+				expect($table).to.contain('test_user (Test User)')
+			})
+		})
+
+	})
+
+	describe('Project Setup', () => {
+
+		before(() => {
+			cy.visit_version({page: '/ProjectSetup/index.php', params: "pid=13"})
+		})
+
+		it('Should show the project without surveys', () => {
+			cy.get('div').contains('Use surveys in this project?').parent().within(($div) => {
+				cy.get('button').contains('Disable').then(($button) => {
+					cy.wrap($button).click().then(($b) => {
+						//Fetch the button again by making a jQuery request
+						let updated_button = Cypress.$($b)
+						cy.get(updated_button).should('contain', 'Disable')
+					})
+				})
+			})
+		})
+
+		it ('Should show the project in development mode', () => {
+			cy.get('div.menubox').should(($div) => {
+				expect($div).to.contain('Project status')
+				expect($div).to.contain('Development')
+			})
+		})
+
+		it('Should show the appropriate options for viewing and designing your data collection instruments', () => {
+			cy.visit_version({page: '/ProjectSetup/index.php', params: "pid=13"})
+
+			// Online Designer
+			cy.get('button').contains('Online Designer')
+
+			// Data Dictionary
+			cy.get('button').contains('Data Dictionary')
+
+			// REDCap Shared Library
+			cy.get('button').contains('REDCap Shared Library')
+
+			// Download PDF of all instruments
+			cy.get('a').contains('Download PDF of all instruments')
+
+			// Download current data dictionary
+			cy.get('a').contains('Download the current Data Dictionary')
+
+			// Check for identifiers
+			cy.get('a').contains('Check For Identifiers')
+		})
+
 	})
 
     describe('Online Designer', () => {
 
     	before(() => {
-    		cy.set_user_type('standard')
-			
-			//Visit Classic Database 
+			//Visit Classic Database
 			cy.visit_version({page: 'Design/online_designer.php', params: "pid=13"})
     	})
 
 
-		it('Should contain Project Setup permisisons for current user', () => {
-
-
-		})
-
-		it('Should show the project without surveys', () => {
-
-		})
-
-		it ('Should show the project in development mode', () => {
-
-
-		})
-
-		it('Should show the appropriate options for viewing and designing your data collection instruments', () => {
-			// Online Designer
-			// Data Dictionary
-			// REDCap Shared Library
-			// Download PDF of all instruments
-			// Download current data dictionary
-			// Check for identifiers
-		})
-
 		describe('Data Collection Instruments', () => {
 
-			it('Should allow a new instrument to be created', () => {
+			before(() => {
+				cy.visit_version({page: 'Design/online_designer.php', params: "pid=13"})
+			})
 
+			it('Should allow a new instrument to be created', () => {
+				cy.get('div').
+					contains('a new instrument from scratch').
+					parent().
+					within(($div) => {
+
+					cy.get('button').contains('Create').click()
+
+				})
+
+				cy.get('button').contains('Add instrument here').click()
+
+				cy.get('td').contains('New instrument name').parent().within(($td) => {
+					cy.get('input[type=text]', {force: true}).type('My Second Instrument')
+					cy.get('input[value=Create]', {force: true}).click()
+				})
+
+				cy.get('span').should(($span) => {
+					expect($span).to.contain('My Second Instrument')
+				})
 			})
 
 			it('Should allow an instrument to be renamed', () => {
+				cy.get('span').contains('My Second Instrument').parents('tr').children('td').last().within(($td) => {
+					cy.get('button').contains('Choose action').click()
+				})
 
-			})
+				cy.get('a').should('contain', 'Rename')
+				cy.get('a').contains('Rename').click()
 
-			it('Should allow an instrument to be deleted', () => {
+				cy.get('input[value="My Second Instrument"]').then(($i) => {
+					cy.wrap($i).clear().type('My Renamed Instrument')
+
+					cy.wrap($i).parent('div').should(($div) => {
+
+						//If there are three inputs, we know that things have finished loading
+						expect($div.children()).to.have.length(3)
+
+						//There might be a more elegant way to select this, but this works for now
+						$div.children('input[type=button]')[0].click()
+					})
+				})
+
+				cy.get('span').should(($span) => {
+					expect($span).to.contain('My Renamed Instrument')
+				})
 
 			})
 
 			it('Should allow instruments to be reordered', () => {
+
+			})
+
+			it('Should allow an instrument to be deleted', () => {
 
 			})
 
