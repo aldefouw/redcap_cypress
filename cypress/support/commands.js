d441f49191cb6781c60c5f4042f18e005abe1051
Adam De Fouw <aldefouw@medicine.wisc.edu>
2022-08-30 16:46:03 -0500

    Implement ability to lock out DB seed on subsequent runs
    This should only be used in development, but for times when you want to favor speed over data, you can use this 'skip_db_seed' key in the mysql array of cypress.env.json.
    
    If you need the database populated again, it's as simple as deleting the .lock file to get it back.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -56,36 +56,54 @@ Cypress.Commands.add('base_db_seed', () => {
         alert('redcap_source_path, which defines where your REDCap source code exists, is missing in cypress.env.json.  Please configure it before proceeding.')
     }
 
-    cy.task('populateStructureAndData', {
-                                            redcap_version: Cypress.env('redcap_version'),
-                                            advanced_user_info: compareVersions.compare(Cypress.env('redcap_version'), '10.1.0', '>='),
-                                            source_location: redcap_source_path
-                                        }).then((structure_and_data_file_exists) => {
+    //Get MySQL array from Environment Variables
+    let mysql = Cypress.env('mysql')
 
-        //Only run this block if the Structure and Data File exists and has gone through proper processes
-        if(structure_and_data_file_exists){
+    // We can skip populating the base db seed on successive runs by adding a false value to the mysql['initial_db_seed'] key in cypress.env.json
+    // This should really ever be run in development but shouldn't necessarily harm production since we still need a DB lock file to proceed
+    cy.task('dbSeedLockExists').then((db_seed_exists) => {
 
-            //Create the database if it doesn't exist
-            cy.mysql_db('create_database', '', false).then(() => {
+        if(
+            !mysql.hasOwnProperty('skip_db_seed') ||
+            mysql.hasOwnProperty('skip_db_seed') && mysql['skip_db_seed'] && !db_seed_exists ||
+            !mysql['skip_db_seed']
+        ) {
 
-                //Pull in the structure and data from REDCap Source
-                cy.mysql_db('structure_and_data', window.base_url).then(() => {
+            cy.task('populateStructureAndData', {
+                redcap_version: Cypress.env('redcap_version'),
+                advanced_user_info: compareVersions.compare(Cypress.env('redcap_version'), '10.1.0', '>='),
+                source_location: redcap_source_path
+            }).then((structure_and_data_file_exists) => {
 
-                    if(Cypress.env('redcap_hooks_path') !== undefined){
-                        const redcap_hooks_path = "REDCAP_HOOKS_PATH/" + Cypress.env('redcap_hooks_path').replace(/\//g, "\\/");
-                        cy.mysql_db('hooks_config', redcap_hooks_path) //Fetch the hooks SQL seed data
-                    }
+                //Only run this block if the Structure and Data File exists and has gone through proper processes
+                if (structure_and_data_file_exists) {
 
-                    //Clear out all cookies
-                    cy.clearCookies()
-                })
+                    //Create the database if it doesn't exist
+                    cy.mysql_db('create_database', '', false).then(() => {
+
+                        //Pull in the structure and data from REDCap Source
+                        cy.mysql_db('structure_and_data', window.base_url).then(() => {
+
+                            if (Cypress.env('redcap_hooks_path') !== undefined) {
+                                const redcap_hooks_path = "REDCAP_HOOKS_PATH/" + Cypress.env('redcap_hooks_path').replace(/\//g, "\\/");
+                                cy.mysql_db('hooks_config', redcap_hooks_path) //Fetch the hooks SQL seed data
+                            }
+
+                            //Clear out all cookies
+                            cy.clearCookies()
+
+                            //Create a DB Seed Lock file
+                            cy.task('createInitialDbSeedLock')
+                        })
+
+                    })
+
+                } else {
+                    alert('Warning: Error generating structure and data file.  This usually happpens because your REDCap source code is missing files.')
+                }
 
             })
-
-        } else {
-            alert('Warning: Error generating structure and data file.  This usually happpens because your REDCap source code is missing files.')
         }
-
     })
 })
 
