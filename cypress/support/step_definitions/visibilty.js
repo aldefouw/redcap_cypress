c700b107cad2f36ed81039b3e7aa3d40c1346cf6
Adam De Fouw <aldefouw@medicine.wisc.edu>
2022-10-05 16:39:19 -0500

    Update visibility

--- cypress/support/step_definitions/visibilty.js
+++ cypress/support/step_definitions/visibilty.js
@@ -48,7 +48,7 @@ Given("I should see a link labeled {string}", (label) => {
  * @module Visibility
  * @author Tintin Nguyen <tin-tin.nguyen@nih.gov>
  * @example I should see a new dialog box named {string}
- * @param {string} text the id of the new dialog box 
+ * @param {string} text the id of the new dialog box
  * @description Visually verifies that there is a new dialog box with the id text
  */
 Given("I should see a new dialog box named {string}", (text) => {
@@ -64,11 +64,11 @@ Given("I should see a new dialog box named {string}", (text) => {
  * @param {string} check - valid choices are 'checked' OR 'unchecked'
  * @description Visually verifies that a specified checkbox is checked or uncheck
  */
- defineParameterType({
+defineParameterType({
     name: 'check',
     regexp: /checked|unchecked/
 })
- Given("I should see the checkbox identified by {string}, {check}", (sel, check) => {
+Given("I should see the checkbox identified by {string}, {check}", (sel, check) => {
     //Really only added this to delay cypress cause sometimes it was moving forward without being checked
     check == 'checked' ? cy.get(sel).should('be.checked') : cy.get(sel).should('not.be.checked')
 })
@@ -82,6 +82,6 @@ Given("I should see a new dialog box named {string}", (text) => {
  * @description Visually verifies that a specified input field is set to text
  */
 
- Given("I should see the input field identified by {string} with the value {string}", (selector, text) => {
+Given("I should see the input field identified by {string} with the value {string}", (selector, text) => {
     cy.get(selector).should("have.value", text)
 })
