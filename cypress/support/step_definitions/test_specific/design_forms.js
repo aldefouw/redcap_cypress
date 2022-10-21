3849c7a8395a1ccf12caf521dd9cf277548c7a2d
cdebacker <corey.debacker@gmail.com>
2022-10-21 17:16:06 -0500

    Added test-specific (7) step definition for verifying multiple choice coding
    Step definition will be refactored later to use a custom Cypress command
    
    Consider moving step definition to online_designer.js (if step is determined to be useful for other tests)

--- cypress/support/step_definitions/test_specific/design_forms.js
+++ cypress/support/step_definitions/test_specific/design_forms.js
@@ -18,3 +18,42 @@ Given('The field types specified in step 20 of script 07 should be available',  
         })
     })
 })
+
+//TODO: refactor to custom cypress command
+//Requires that the current page is the instrument editing page within the online designer
+//Current implementation does not support matrix variables
+Given('I should see that the choice {string} for the field {string} is coded as {string}', (choice_text, field_name, code) => {
+
+    cy.get(`#${field_name}-tr`).within(() => {
+        cy.get(`label:contains(${choice_text}), option:contains(${choice_text})`).then($e => {
+            //filter matches for exact text match, in case one choice is a substring of another
+            $e = $e.filter(function() {
+                return (
+                    cy.$$(this).text() === choice_text
+                    && ! cy.$$(this).hasClass('fl')) //don't include field label
+            })
+
+            let foundCorrectCode = false
+            
+            if ($e.prop('tagName') === 'OPTION') {
+                if ($e.attr('value') === code) {
+                    foundCorrectCode = true
+                }
+            } else if ($e.prop('tagName') === 'LABEL') {
+                let $i = $e.parent().find('input:visible')
+                let type = $i.attr('type')
+                if (type === 'checkbox') {
+                    if ($i.attr('code') === code) {
+                        foundCorrectCode = true
+                    }
+                } else if (type === 'radio') {
+                    if ($i.attr('value') === code) {
+                        foundCorrectCode = true
+                    }
+                }
+            }
+
+            expect(foundCorrectCode).to.be.true
+        })
+    })
+})
