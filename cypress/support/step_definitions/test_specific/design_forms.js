45dd24bffbf6a058fbac1c27bb0edfc8e7b0fded
cdebacker <corey.debacker@gmail.com>
2022-10-21 13:51:39 -0500

    Added test-specific step definition for test script 7, step 20
    See commit title

--- cypress/support/step_definitions/test_specific/design_forms.js
+++ cypress/support/step_definitions/test_specific/design_forms.js
@@ -1,5 +1,12 @@
 import { Given } from "cypress-cucumber-preprocessor/steps";
 
+/**
+ * @module DesignFormsTests
+ * @author Corey DeBacker <debacker@wisc.edu>
+ * @description Asserts that 'Text Box', 'Notes Box', 'Calculated Field', 'Multiple Choice - Drop-down', 
+ * 'Multiple Choice - Radio', 'Checkboxes', 'Signature', 'File Upload', 'Descriptive Text', and 'Begin New Section' 
+ * are all available choices in the field type dropdown when adding a new field to an instrument.
+ */
 Given('The field types specified in step 20 of script 07 should be available',  () => {
     let options = [
         'Text Box', 'Notes Box', 'Calculated Field', 'Multiple Choice - Drop-down', 'Multiple Choice - Radio',
