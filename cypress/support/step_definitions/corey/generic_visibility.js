d275a47818ed95556848e3b9e3aec9e8e8614889
cdebacker <corey.debacker@gmail.com>
2023-01-12 18:59:18 -0600

    Disabled /step_definitions/corey/ files
    Commented out all lines of code contained in /step_definitions/corey/ (except for temp_109_steps.js) to prevent overriding other step definitions. Considering deleting the directory altogether, but I would like to keep it for now until I figure out the best way to store old code that I might want to reference later, in a way that is convenient for my development setup.

--- cypress/support/step_definitions/corey/generic_visibility.js
+++ cypress/support/step_definitions/corey/generic_visibility.js
@@ -1,5 +1,5 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
+// import { Given } from "cypress-cucumber-preprocessor/steps";
 
-Given(/^I should see an element identified by "(.*?)"(?: containing the text "(.*)")?$/, (sel, text = '') => {
-    cy.get(`${sel}:contains("${text}")`).should('be.visible')
-})
+// Given(/^I should see an element identified by "(.*?)"(?: containing the text "(.*)")?$/, (sel, text = '') => {
+//     cy.get(`${sel}:contains("${text}")`).should('be.visible')
+// })
--- cypress/support/step_definitions/corey/project_setup.js
+++ cypress/support/step_definitions/corey/project_setup.js
@@ -1,68 +1,67 @@
-import { Given } from "cypress-cucumber-preprocessor/steps";
+// import { Given } from "cypress-cucumber-preprocessor/steps";
 
-Given("I should see that surveys are {string}", (state) => {
+// Given("I should see that surveys are {string}", (state) => {
 
-    let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
-    cy.get('#setupEnableSurveysBtn').should('contain.text', expected_text);
+//     let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
+//     cy.get('#setupEnableSurveysBtn').should('contain.text', expected_text);
 
-})
+// })
 
-Given("I should see that longitudinal mode is {string}", (state) => {
+// Given("I should see that longitudinal mode is {string}", (state) => {
 
-    let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
-    cy.get('#setupLongiBtn').should('contain.text', expected_text);
+//     let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
+//     cy.get('#setupLongiBtn').should('contain.text', expected_text);
 
-})
+// })
 
-Given("I should see that repeatable instruments are {string}", (state) => {
+// Given("I should see that repeatable instruments are {string}", (state) => {
 
-    let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
-    cy.get('#enableRepeatingFormsEventsBtn').should('contain.text', expected_text);
+//     let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
+//     cy.get('#enableRepeatingFormsEventsBtn').should('contain.text', expected_text);
 
-})
+// })
 
-Given("I should see that auto-numbering is {string}", (state) => {
+// Given("I should see that auto-numbering is {string}", (state) => {
 
-    let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
-    cy.contains('Auto-numbering for records').within($div => {
-        cy.get('button').should('contain.text', expected_text);
-    })
+//     let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
+//     cy.contains('Auto-numbering for records').within($div => {
+//         cy.get('button').should('contain.text', expected_text);
+//     })
 
-})
+// })
 
-Given("I should see that the scheduling module is {string}", (state) => {
+// Given("I should see that the scheduling module is {string}", (state) => {
 
-    let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
-    cy.contains('Scheduling module').within($div => {
-        cy.get('button').should('contain.text', expected_text);
-    })
+//     let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
+//     cy.contains('Scheduling module').within($div => {
+//         cy.get('button').should('contain.text', expected_text);
+//     })
 
-})
+// })
 
-Given("I should see that the randomization module is {string}", (state) => {
+// Given("I should see that the randomization module is {string}", (state) => {
 
-    let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
-    cy.contains('Randomization module').within($div => {
-        cy.get('button').should('contain.text', expected_text);
-    })
+//     let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
+//     cy.contains('Randomization module').within($div => {
+//         cy.get('button').should('contain.text', expected_text);
+//     })
 
-})
+// })
 
-Given("I should see that the designate an email for communications setting is {string}", (state) => {
+// Given("I should see that the designate an email for communications setting is {string}", (state) => {
 
-    let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
-    cy.contains('Designate an email for communications').within($div => {
-        cy.get('button').should('contain.text', expected_text);
-    })
+//     let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
+//     cy.contains('Designate an email for communications').within($div => {
+//         cy.get('button').should('contain.text', expected_text);
+//     })
+// })
 
-})
+// Given("I should see that the optional module {string} is {string}", (mod_text, state) => {
 
-Given("I should see that the optional module {string} is {string}", (mod_text, state) => {
+//     let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
+//     cy.get('#setupChklist-modules > table > tbody > tr > > > div')
+//         .contains(mod_text, {matchCase: false}).within($div => {
+//             cy.get('button').should('contain.text', expected_text);
+//         })
 
-    let expected_text = ((state.toLowerCase() === 'enabled') ? 'Disable' : 'Enable');
-    cy.get('#setupChklist-modules > table > tbody > tr > > > div')
-        .contains(mod_text, {matchCase: false}).within($div => {
-            cy.get('button').should('contain.text', expected_text);
-        })
-
-})
+// })
