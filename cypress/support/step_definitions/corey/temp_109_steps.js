1f7da0f59fc90d80ea719866e0623a4d13f92b2d
cdebacker <corey.debacker@gmail.com>
2023-01-16 15:08:12 -0600

    Changed "nearest" to "near" in new step definitions for issue #109

--- cypress/support/step_definitions/corey/temp_109_steps.js
+++ cypress/support/step_definitions/corey/temp_109_steps.js
@@ -16,7 +16,7 @@ defineParameterType({
 })
 
 // Compare against the commented-out, less abstract step definitions at the end of the file. I think this is preferable.
-Given('I click on the {ordinal}{clickable} nearest the text {string}', (n, type, text) => {
+Given('I click on the {ordinal}{clickable} near the text {string}', (n, type, text) => {
     let subsels = {link: 'a', button: 'button', checkbox: 'input[type=checkbox]', radio: 'input[type=radio]'}
     let subsel = subsels[type]
     let sel = `:contains(${text}):has(${subsel}):not(:has(:contains(${text}):has(${subsel})))`
@@ -38,7 +38,7 @@ Given('I select {string} from the {ordinal}dropdown nearest the text {string}', 
         })
 })
 
-// Given(/^I click on the(?: (first|second|third|fourth|fifth|sixth|seventh|eighth|last))? link nearest the text "(.*)"/, (n, text) => {
+// Given(/^I click on the(?: (first|second|third|fourth|fifth|sixth|seventh|eighth|last))? link near the text "(.*)"/, (n, text) => {
 //     n = ordinal_to_int(n)
 //     let sel = `:contains(${text}):has(a):not(:has(:contains(${text}):has(a)))`
 //     cy.get_top_layer(($el) => {expect($el.find(sel)).length.to.be.above(0)})
@@ -49,7 +49,7 @@ Given('I select {string} from the {ordinal}dropdown nearest the text {string}', 
 //         })
 // })
 
-// Given(/^I click on the(?: (first|second|third|fourth|fifth|sixth|seventh|eighth|last))? button nearest the text "(.*)"/, (n, text) => {
+// Given(/^I click on the(?: (first|second|third|fourth|fifth|sixth|seventh|eighth|last))? button near the text "(.*)"/, (n, text) => {
 //     n = ordinal_to_int(n)
 //     let sel = `:contains(${text}):has(button):not(:has(:contains(${text}):has(button)))`
 //     cy.get_top_layer(($el) => {expect($el.find(sel)).length.to.be.above(0)})
@@ -60,7 +60,7 @@ Given('I select {string} from the {ordinal}dropdown nearest the text {string}', 
 //         })
 // })
 
-// Given(/^I click on the(?: (first|second|third|fourth|fifth|sixth|seventh|eighth|last))? checkbox nearest the text "(.*)"/, (n, text) => {
+// Given(/^I click on the(?: (first|second|third|fourth|fifth|sixth|seventh|eighth|last))? checkbox near the text "(.*)"/, (n, text) => {
 //     n = ordinal_to_int(n)
 //     let sel = `:contains(${text}):has(input[type=checkbox]):not(:has(:contains(${text}):has(input[type=checkbox])))`
 //     cy.get_top_layer(($el) => {expect($el.find(sel)).length.to.be.above(0)})
