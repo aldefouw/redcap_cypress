b11bc92190544fa350ad0976e7f0effc42cf9e8f
Adam De Fouw <aldefouw@medicine.wisc.edu>
2023-02-27 14:35:20 -0600

    Expand to include SPAN and DIV for detachment prevention
    Maybe this will help.

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1142,7 +1142,7 @@ Cypress.Commands.add('ensure_csrf_token', () => {
                             //If our cookies include PHPSESSID, we can assume we're logged into REDCap
                             //If they do NOT include PHPSESSID, we shouldn't have to worry about this token
                             //It also appears that the Report Forms DO not need a CSRF token, which is interesting ...
-                            if($cookie['name'] === 'PHPSESSID' && Cypress.$('form#create_report_form').length === 0){
+                            if($cookie['name'] === 'PHPSESSID'){
                                 cy.get('form input[name=redcap_csrf_token]').each(($form_token) => {
                                     cy.window().then((win) => {
                                         expect($form_token[0].value).to.not.be.null
@@ -1181,10 +1181,13 @@ Cypress.Commands.overwrite(
 
             if(subject[0].nodeName === "A" ||
                 subject[0].nodeName === "BUTTON" ||
-                subject[0].nodeName === "INPUT" && subject[0].type === "button" && subject[0].onclick === ""){
+                subject[0].nodeName === "INPUT" && subject[0].type === "button" && subject[0].onclick === "" ||
+                subject[0].nodeName === "SPAN" ||
+                subject[0].nodeName === "DIV"
+            ){
 
                 //Is the element part of a form?
-                if(subject[0].form){
+                if(subject[0].form && Cypress.$('form#create_report_form').length === 0){
                     cy.ensure_csrf_token() //Check for the CSRF token to be set in the form
                 }
 
