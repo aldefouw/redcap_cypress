5c5d3ad76f9e11db18bdb12f8deb38093fc852ce
Adam De Fouw <aldefouw@medicine.wisc.edu>
2023-03-10 16:00:44 -0600

    More adjustments to step definitions

--- cypress/support/step_definitions/download.js
+++ cypress/support/step_definitions/download.js
@@ -10,7 +10,7 @@ import {Given} from "cypress-cucumber-preprocessor/steps";
 Given("I download a file by clicking on the link labeled {string}", (text) => {
     // We do not actually click on the link because new windows and Cypress do not work.
     // Instead, we sideload a request and save it where it would go
-    cy.get('a').contains(text).should('be.visible').then((f) => {
+    cy.get('a:contains(' + text + '):visible').then((f) => {
         cy.request({
             url: f[0]['href'],
             encoding: 'binary'
