df2057d0edbc27f93c456715e68a873af5374d1b
Adam De Fouw <aldefouw@medicine.wisc.edu>
2023-03-16 15:31:02 -0500

    Visit REDCap login page

---
+++ cypress/support/step_definitions/visit_page.js
@@ -0,0 +1,12 @@
+import { Given } from "cypress-cucumber-preprocessor/steps";
+
+/**
+ * @module VisitPage
+ * @author Adam De Fouw <aldefouw@medicine.wisc.edu>
+ * @example I visit the REDCap login page
+ * @description Instructs Cypress to the REDCpa login page
+ */
+Given("I visit the REDCap login page", () => {
+    cy.logout()
+    cy.visit_version({page: '/'})
+})
