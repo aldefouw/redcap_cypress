7086ed29da4c29b16c39349f8e29f68096a8b5ec
Adam De Fouw <aldefouw@medicine.wisc.edu>
2023-03-16 20:02:34 -0500

    Squashed
    Update dialog.js
    
    Add a guard
    
    Revert "Add a guard"
    
    This reverts commit 0f40308f95a2df5482d8e55cf9054f9b6fb13100.
    
    Barking up wrong tree

--- cypress/support/core/commands/dialog.js
+++ cypress/support/core/commands/dialog.js
@@ -19,5 +19,17 @@ Cypress.Commands.add("click_on_dialog_button", (text, selector = 'button') => {
 })
 
 Cypress.Commands.add("verify_text_on_dialog", (text) => {
-    cy.get('div[role="dialog"]:visible').should('contain', text)
+    cy.get('div[role="dialog"]').then((divs) => {
+        // can be multiple layers of dialogs, find the top most - tintin edit
+        let topDiv = null
+        for(let i = 0; i < divs.length; i++){
+            // ignore invisible dialogs
+            if(divs[i].style.display === 'none') {continue}
+
+            if(topDiv == null || divs[i].style.zIndex > topDiv.style.zIndex){
+                topDiv = divs[i]
+            }
+        }
+        cy.wrap(topDiv).should('contain', text)
+    })
 })
