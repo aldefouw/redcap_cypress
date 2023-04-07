055f8b014ef4bd4a843d20468c26fe7664a20e16
Adam De Fouw <aldefouw@medicine.wisc.edu>
2023-04-07 16:33:07 -0500

    Support files

--- cypress/support/step_definitions/user_rights.js
+++ cypress/support/step_definitions/user_rights.js
@@ -418,24 +418,8 @@ Given('I {user_right_action} all Basic Rights within the open User Rights dialog
  */
 Given('I set Data Viewing Rights to {data_viewing_rights} for the instrument {string}', (level, instrument) => {
     cy.get('div[role=dialog]').should('be.visible')
-    let input_values = {'No Access': 0, 'Read Only': 2, 'View & Edit': 1}
-    cy.get(`table#form_rights tr:has(td:contains(${instrument})) input[value=${input_values[level]}]:visible`)
-        .check()
-})
-
-
-/**
- * @module UserRights
- * @author Corey DeBacker <debacker@wisc.edu>
- * @example I set Data Viewing Rights to < No Access | Read Only | View & Edit > for the instrument {string}
- * @param {data_viewing_rights} level - the level of rights to be assigned
- * @param {string} instrument - the label of the instrument for which to configure data entry rights
- * @description Selects a radio option for Data Entry Rights for the specified instrument within the user rights configuration dialog.
- */
-Given('I ', (level, instrument) => {
-    cy.get('div[role=dialog]').should('be.visible')
-    let input_values = {'No Access': 0, 'Read Only': 2, 'View & Edit': 1}
-    cy.get(`table#form_rights tr:has(td:contains(${instrument})) input[value=${input_values[level]}]:visible`)
+    let selectors = {'No Access': 'input[value=0]', 'Read Only': 'input[value=2]', 'View & Edit': 'input[value=1]', 'Edit survey responses': 'input[type=checkbox]'}
+    cy.get(`table#form_rights tr:has(td:contains(${instrument})) ${selectors[level]}`)
         .check()
 })
 
