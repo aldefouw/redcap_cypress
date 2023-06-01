4c90b573e8b12f768ea950dcd4e05367e66858f1
Adam De Fouw <aldefouw@medicine.wisc.edu>
2023-06-01 11:15:01 -0500

    Missing step definition

--- cypress/support/step_definitions/test_specific/add_manage_users.js
+++ cypress/support/step_definitions/test_specific/add_manage_users.js
@@ -3,9 +3,9 @@ import {Given} from "cypress-cucumber-preprocessor/steps";
 /**
  * @module AddManageUsers
  * @author Adam De Fouw <aldefouw@medicine.wisc.edu>
- * @example I wait for one minute
- * @description Waits for a minute before allowing anything else to happen
+ * @example I wait for {integer} minute(s)
+ * @description Waits for specified number of minutes before allowing anything else to happen
  */
-Given("I wait for one minute", () => {
-    cy.wait(60000)
+Given("I wait for {int} minute(s)", (minutes) => {
+    cy.wait(minutes * 60000)
 })
