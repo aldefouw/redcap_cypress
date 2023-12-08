eaabc65107be151172119fe881c7f878004972ad
Adam De Fouw <aldefouw@medicine.wisc.edu>
2023-12-08 15:45:50 -0600

    Update documentation

--- docs/assets/rctf_versions.js
+++ docs/assets/rctf_versions.js
@@ -1,4 +1,4 @@
-const rctf_versions = ['v1.0.18.html','v1.0.19.html','v1.0.20.html']
+const rctf_versions = ['v1.0.18.html','v1.0.19.html','v1.0.20.html','v1.0.21.html']
   window.versions = ''
 rctf_versions.forEach(function(v) {
   window.versions += `<li><a href="${v}">${v.split(".html")[0]}</a></li>`
